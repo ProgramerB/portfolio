bb2063403a7e19e0a5e15f9d181db3a1f9571a52
ProgramerB <Bhavesh2000@outlook.com>
2024-02-13 11:53:54 +0530

    added work images

--- src/ItemModel.ts
+++ src/ItemModel.ts
@@ -1,9 +1,53 @@
 import leaf from './img/leaf.png'
 import bike from './img/bike.png'
 import train from './img/train.png'
-import city from './img/city.png'
-import horse from './img/horse.png'
-import plant from './img/plant.png'
+// import city from './img/city.png'
+// import horse from './img/horse.png'
+// import plant from './img/plant.png'
+
+import work_1 from './img/work/1.jpg'
+import work_2 from './img/work/2.jpg'
+import work_3 from './img/work/3.jpg'
+import work_4 from './img/work/4.jpg'
+import work_5 from './img/work/5.jpg'
+import work_6 from './img/work/6.jpg'
+import work_7 from './img/work/7.jpg'
+import work_8 from './img/work/8.jpg'
+import work_9 from './img/work/9.jpg'
+import work_10 from './img/work/10.png'
+
+import work_11 from './img/work/11.png'
+import work_12 from './img/work/12.png'
+import work_13 from './img/work/13.png'
+import work_14 from './img/work/14.png'
+import work_15 from './img/work/15.jpg'
+import work_16 from './img/work/16.png'
+import work_17 from './img/work/17.png'
+import work_18 from './img/work/18.png'
+import work_19 from './img/work/19.png'
+import work_20 from './img/work/20.png'
+
+import work_21 from './img/work/21.png'
+import work_22 from './img/work/22.png'
+import work_23 from './img/work/23.png'
+import work_24 from './img/work/24.png'
+import work_25 from './img/work/25.png'
+import work_26 from './img/work/26.png'
+import work_27 from './img/work/27.jpg'
+import work_28 from './img/work/28.jpg'
+import work_29 from './img/work/29.jpg'
+import work_30 from './img/work/30.png'
+
+import work_31 from './img/work/31.png'
+import work_32 from './img/work/32.png'
+import work_33 from './img/work/33.png'
+import work_34 from './img/work/34.png'
+import work_35 from './img/work/35.jpg'
+import work_36 from './img/work/36.png'
+import work_37 from './img/work/37.png'
+import work_38 from './img/work/38.png'
+import work_39 from './img/work/39.png'
+
 export type ItemData = {
     id: number
     name: string
@@ -15,39 +59,238 @@ export const items:ItemData[] = [
     {
         id:1,
         name: 'Dewdrop Leaf',
-        image: leaf,
+        image: work_1,
         description: 'A leaf'
     },
     {
         id:2,
         name: "Rural Bike",
-        image: bike,
+        image: work_2,
         description: 'Boke'
     },
     {
         id:3,
         name: "Train Ride",
-        image: train,
+        image: work_3,
         description: 'Chhu Chu'
     },
     {
         id:4,
         name: "Citylines",
-        image: city,
+        image: work_4,
         description: 'lights and action'
     },
     {
         id:5,
         name: "Horse",
-        image: horse,
+        image: work_5,
         description: 'Bojack'
     },
     {
         id:6,
         name: "Plant",
-        image: plant,
+        image: work_6,
         description: 'I am groot'
     },
+    {
+        id:7,
+        name: "Plant",
+        image: work_7,
+        description: 'I am groot'
+    },
+    {
+        id:8,
+        name: "Plant",
+        image: work_8,
+        description: 'I am groot'
+    },
+    {
+        id:9,
+        name: "Plant",
+        image: work_9,
+        description: 'I am groot'
+    },
+    {
+        id:10,
+        name: "Plant",
+        image: work_10,
+        description: 'I am groot'
+    },
+    {
+        id:11,
+        name: 'Dewdrop Leaf',
+        image: work_11,
+        description: 'A leaf'
+    },
+    {
+        id:12,
+        name: "Rural Bike",
+        image: work_12,
+        description: 'Boke'
+    },
+    {
+        id:13,
+        name: "Train Ride",
+        image: work_13,
+        description: 'Chhu Chu'
+    },
+    {
+        id:14,
+        name: "Citylines",
+        image: work_14,
+        description: 'lights and action'
+    },
+    {
+        id:15,
+        name: "Horse",
+        image: work_15,
+        description: 'Bojack'
+    },
+    {
+        id:16,
+        name: "Plant",
+        image: work_16,
+        description: 'I am groot'
+    },
+    {
+        id:17,
+        name: "Plant",
+        image: work_17,
+        description: 'I am groot'
+    },
+    {
+        id:18,
+        name: "Plant",
+        image: work_18,
+        description: 'I am groot'
+    },
+    {
+        id:19,
+        name: "Plant",
+        image: work_19,
+        description: 'I am groot'
+    },
+    {
+        id:20,
+        name: "Plant",
+        image: work_20,
+        description: 'I am groot'
+    },
+    {
+        id:21,
+        name: 'Dewdrop Leaf',
+        image: work_21,
+        description: 'A leaf'
+    },
+    {
+        id:22,
+        name: "Rural Bike",
+        image: work_22,
+        description: 'Boke'
+    },
+    {
+        id:23,
+        name: "Train Ride",
+        image: work_23,
+        description: 'Chhu Chu'
+    },
+    {
+        id:24,
+        name: "Citylines",
+        image: work_24,
+        description: 'lights and action'
+    },
+    {
+        id:25,
+        name: "Horse",
+        image: work_25,
+        description: 'Bojack'
+    },
+    {
+        id:26,
+        name: "Plant",
+        image: work_26,
+        description: 'I am groot'
+    },
+    {
+        id:27,
+        name: "Plant",
+        image: work_27,
+        description: 'I am groot'
+    },
+    {
+        id:28,
+        name: "Plant",
+        image: work_28,
+        description: 'I am groot'
+    },
+    {
+        id:29,
+        name: "Plant",
+        image: work_29,
+        description: 'I am groot'
+    },
+    {
+        id:30,
+        name: "Plant",
+        image: work_30,
+        description: 'I am groot'
+    },
+    {
+        id:31,
+        name: 'Dewdrop Leaf',
+        image: work_31,
+        description: 'A leaf'
+    },
+    {
+        id:32,
+        name: "Rural Bike",
+        image: work_32,
+        description: 'Boke'
+    },
+    {
+        id:33,
+        name: "Train Ride",
+        image: work_33,
+        description: 'Chhu Chu'
+    },
+    {
+        id:34,
+        name: "Citylines",
+        image: work_34,
+        description: 'lights and action'
+    },
+    {
+        id:35,
+        name: "Horse",
+        image: work_35,
+        description: 'Bojack'
+    },
+    {
+        id:36,
+        name: "Plant",
+        image: work_36,
+        description: 'I am groot'
+    },
+    {
+        id:37,
+        name: "Plant",
+        image: work_37,
+        description: 'I am groot'
+    },
+    {
+        id:38,
+        name: "Plant",
+        image: work_38,
+        description: 'I am groot'
+    },
+    {
+        id:39,
+        name: "Plant",
+        image: work_39,
+        description: 'I am groot'
+    },
+
 ]
 
 export const hobbyItems:ItemData[] = [

26830d0eb52b6ad912fc543b20691bac4359b219
ProgramerB <Bhavesh2000@outlook.com>
2024-02-13 12:46:11 +0530

    arranged images

--- src/ItemModel.ts
+++ src/ItemModel.ts
@@ -5,48 +5,50 @@ import train from './img/train.png'
 // import horse from './img/horse.png'
 // import plant from './img/plant.png'
 
-import work_1 from './img/work/1.jpg'
-import work_2 from './img/work/2.jpg'
-import work_3 from './img/work/3.jpg'
-import work_4 from './img/work/4.jpg'
-import work_5 from './img/work/5.jpg'
-import work_6 from './img/work/6.jpg'
-import work_7 from './img/work/7.jpg'
-import work_8 from './img/work/8.jpg'
-import work_9 from './img/work/9.jpg'
-import work_10 from './img/work/10.png'
+import shirt_1 from './img/work/shirt/1.jpg'
+import shirt_2 from './img/work/shirt/2.jpg'
+import shirt_3 from './img/work/shirt/3.jpg'
+import shirt_4 from './img/work/shirt/4.jpg'
+import shirt_5 from './img/work/shirt/5.png'
+import shirt_6 from './img/work/shirt/6.png'
+import shirt_7 from './img/work/shirt/7.jpg'
+import shirt_8 from './img/work/shirt/8.png'
+import shirt_9 from './img/work/shirt/9.png'
+import shirt_10 from './img/work/shirt/10.png'
 
-import work_11 from './img/work/11.png'
-import work_12 from './img/work/12.png'
-import work_13 from './img/work/13.png'
-import work_14 from './img/work/14.png'
-import work_15 from './img/work/15.jpg'
-import work_16 from './img/work/16.png'
-import work_17 from './img/work/17.png'
-import work_18 from './img/work/18.png'
-import work_19 from './img/work/19.png'
-import work_20 from './img/work/20.png'
+import shirt_11 from './img/work/shirt/11.png'
+import shirt_12 from './img/work/shirt/12.png'
+import shirt_13 from './img/work/shirt/13.png'
+import shirt_14 from './img/work/shirt/14.png'
+import shirt_15 from './img/work/shirt/15.png'
+import shirt_16 from './img/work/shirt/16.png'
+import shirt_17 from './img/work/shirt/17.png'
+import shirt_18 from './img/work/shirt/18.png'
+import shirt_19 from './img/work/shirt/19.png'
+import shirt_20 from './img/work/shirt/20.png'
 
-import work_21 from './img/work/21.png'
-import work_22 from './img/work/22.png'
-import work_23 from './img/work/23.png'
-import work_24 from './img/work/24.png'
-import work_25 from './img/work/25.png'
-import work_26 from './img/work/26.png'
-import work_27 from './img/work/27.jpg'
-import work_28 from './img/work/28.jpg'
-import work_29 from './img/work/29.jpg'
-import work_30 from './img/work/30.png'
+import shirt_21 from './img/work/shirt/21.jpg'
+import shirt_22 from './img/work/shirt/22.png'
+import shirt_23 from './img/work/shirt/23.png'
 
-import work_31 from './img/work/31.png'
-import work_32 from './img/work/32.png'
-import work_33 from './img/work/33.png'
-import work_34 from './img/work/34.png'
-import work_35 from './img/work/35.jpg'
-import work_36 from './img/work/36.png'
-import work_37 from './img/work/37.png'
-import work_38 from './img/work/38.png'
-import work_39 from './img/work/39.png'
+
+import book_1 from './img/work/book/1.jpg'
+import book_2 from './img/work/book/2.jpg'
+import book_3 from './img/work/book/3.jpg'
+import book_4 from './img/work/book/4.jpg'
+import book_5 from './img/work/book/5.jpg'
+import book_6 from './img/work/book/6.png'
+import book_7 from './img/work/book/7.png'
+import book_8 from './img/work/book/8.png'
+import book_9 from './img/work/book/9.png'
+import book_10 from './img/work/book/10.jpg'
+import book_11 from './img/work/book/11.jpg'
+import book_12 from './img/work/book/12.jpg'
+import book_13 from './img/work/book/13.png'
+
+import other_work_1 from './img/work/1.png'
+import other_work_2 from './img/work/2.png'
+import other_work_3 from './img/work/3.png'
 
 export type ItemData = {
     id: number
@@ -59,235 +61,235 @@ export const items:ItemData[] = [
     {
         id:1,
         name: 'Dewdrop Leaf',
-        image: work_1,
+        image: shirt_1,
         description: 'A leaf'
     },
     {
         id:2,
         name: "Rural Bike",
-        image: work_2,
+        image: shirt_2,
         description: 'Boke'
     },
     {
         id:3,
         name: "Train Ride",
-        image: work_3,
+        image: shirt_3,
         description: 'Chhu Chu'
     },
     {
         id:4,
         name: "Citylines",
-        image: work_4,
+        image: shirt_4,
         description: 'lights and action'
     },
     {
         id:5,
         name: "Horse",
-        image: work_5,
+        image: shirt_5,
         description: 'Bojack'
     },
     {
         id:6,
         name: "Plant",
-        image: work_6,
+        image: shirt_6,
         description: 'I am groot'
     },
     {
         id:7,
         name: "Plant",
-        image: work_7,
+        image: shirt_7,
         description: 'I am groot'
     },
     {
         id:8,
         name: "Plant",
-        image: work_8,
+        image: shirt_8,
         description: 'I am groot'
     },
     {
         id:9,
         name: "Plant",
-        image: work_9,
+        image: shirt_9,
         description: 'I am groot'
     },
     {
         id:10,
         name: "Plant",
-        image: work_10,
+        image: shirt_10,
         description: 'I am groot'
     },
     {
         id:11,
         name: 'Dewdrop Leaf',
-        image: work_11,
+        image: shirt_11,
         description: 'A leaf'
     },
     {
         id:12,
         name: "Rural Bike",
-        image: work_12,
+        image: shirt_12,
         description: 'Boke'
     },
     {
         id:13,
         name: "Train Ride",
-        image: work_13,
+        image: shirt_13,
         description: 'Chhu Chu'
     },
     {
         id:14,
         name: "Citylines",
-        image: work_14,
+        image: shirt_14,
         description: 'lights and action'
     },
     {
         id:15,
         name: "Horse",
-        image: work_15,
+        image: shirt_15,
         description: 'Bojack'
     },
     {
         id:16,
         name: "Plant",
-        image: work_16,
+        image: shirt_16,
         description: 'I am groot'
     },
     {
         id:17,
         name: "Plant",
-        image: work_17,
+        image: shirt_17,
         description: 'I am groot'
     },
     {
         id:18,
         name: "Plant",
-        image: work_18,
+        image: shirt_18,
         description: 'I am groot'
     },
     {
         id:19,
         name: "Plant",
-        image: work_19,
+        image: shirt_19,
         description: 'I am groot'
     },
     {
         id:20,
         name: "Plant",
-        image: work_20,
+        image: shirt_20,
         description: 'I am groot'
     },
     {
         id:21,
         name: 'Dewdrop Leaf',
-        image: work_21,
+        image: shirt_21,
         description: 'A leaf'
     },
     {
         id:22,
         name: "Rural Bike",
-        image: work_22,
+        image: shirt_22,
         description: 'Boke'
     },
     {
         id:23,
         name: "Train Ride",
-        image: work_23,
+        image: shirt_23,
         description: 'Chhu Chu'
     },
     {
         id:24,
         name: "Citylines",
-        image: work_24,
+        image: book_1,
         description: 'lights and action'
     },
     {
         id:25,
         name: "Horse",
-        image: work_25,
+        image: book_2,
         description: 'Bojack'
     },
     {
         id:26,
         name: "Plant",
-        image: work_26,
+        image: book_3,
         description: 'I am groot'
     },
     {
         id:27,
         name: "Plant",
-        image: work_27,
+        image: book_4,
         description: 'I am groot'
     },
     {
         id:28,
         name: "Plant",
-        image: work_28,
+        image: book_5,
         description: 'I am groot'
     },
     {
         id:29,
         name: "Plant",
-        image: work_29,
+        image: book_6,
         description: 'I am groot'
     },
     {
         id:30,
         name: "Plant",
-        image: work_30,
+        image: book_7,
         description: 'I am groot'
     },
     {
         id:31,
         name: 'Dewdrop Leaf',
-        image: work_31,
+        image: book_8,
         description: 'A leaf'
     },
     {
         id:32,
         name: "Rural Bike",
-        image: work_32,
+        image: book_9,
         description: 'Boke'
     },
     {
         id:33,
         name: "Train Ride",
-        image: work_33,
+        image: book_10,
         description: 'Chhu Chu'
     },
     {
         id:34,
         name: "Citylines",
-        image: work_34,
+        image: book_11,
         description: 'lights and action'
     },
     {
         id:35,
         name: "Horse",
-        image: work_35,
+        image: book_12,
         description: 'Bojack'
     },
     {
         id:36,
         name: "Plant",
-        image: work_36,
+        image: book_13,
         description: 'I am groot'
     },
     {
         id:37,
         name: "Plant",
-        image: work_37,
+        image: other_work_1,
         description: 'I am groot'
     },
     {
         id:38,
         name: "Plant",
-        image: work_38,
+        image: other_work_2,
         description: 'I am groot'
     },
     {
         id:39,
         name: "Plant",
-        image: work_39,
+        image: other_work_3,
         description: 'I am groot'
     },
 

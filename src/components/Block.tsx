fe43e3648105b7e9f15c5510182e6231d0dc3daa
ProgramerB <Bhavesh2000@outlook.com>
2024-02-14 16:24:18 +0530

    added spinner

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,36 +1,45 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ItemData } from '../ItemModel';
 import './styles.css';
 import {useLocation, useNavigate} from "react-router-dom";
 
 const Block = (item:ItemData) => {
-  let navigate = useNavigate() 
+  let navigate = useNavigate()
   let loc = useLocation()
 
   const [loading, setLoading] = useState(true);
 
-  const handleImageLoad = () => {
-    setLoading(false);
-  };
+  useEffect(() => {
+    const img = new Image();
+    img.onload = () => {
+      setLoading(false);
+    };
+    img.src = item.image;
+  }, [item.image]);
 
   return (
-    <div style={{
-      backgroundImage: "url("+item.image+")",
-    }} className={loading?'block':'none'} onClick={()=>{
-        if(loc.pathname === "/")
-        {
-          navigate("item/"+item.id);
+    <div
+      style={{
+        backgroundImage: loading ? "none" : `url(${item.image})`,
+      }}
+      className='block'
+      onClick={() => {
+        if (loc.pathname === "/") {
+          navigate("item/" + item.id);
+        } else {
+          navigate(loc.pathname + "/" + item.id);
         }
-        else
-        {
-          navigate(loc.pathname+"/"+item.id);
-        }
-    }} onLoad={handleImageLoad}>
+      }}
+    >
+      <div>
+        { loading && (<div className='spinner'></div>)}
+      </div>
       <div className="headingBlock">
         {item.name}
       </div>
     </div>
   )
 }
+
 
 export default Block

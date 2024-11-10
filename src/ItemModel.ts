fd35db63417fb56120317fb96515d591a44bc48f
ProgramerB <Bhavesh2000@outlook.com>
2024-11-10 10:12:05 +0000

    added images and refactored

--- src/ItemModel.ts
+++ src/ItemModel.ts
@@ -31,7 +31,6 @@ import shirt_21 from './img/work/shirt/21.jpg'
 import shirt_22 from './img/work/shirt/22.png'
 import shirt_23 from './img/work/shirt/23.png'
 
-
 import book_1 from './img/work/book/1.jpg'
 import book_2 from './img/work/book/2.jpg'
 import book_3 from './img/work/book/3.jpg'
@@ -50,7 +49,6 @@ import other_work_1 from './img/work/1.png'
 import other_work_2 from './img/work/2.png'
 import other_work_3 from './img/work/3.png'
 
-
 import lens_1 from './img/lens/1.jpg'
 import lens_2 from './img/lens/2.jpg'
 import lens_3 from './img/lens/3.jpg'
@@ -67,6 +65,25 @@ import lens_13 from './img/lens/13.jpg'
 import lens_14 from './img/lens/14.jpg'
 import lens_15 from './img/lens/15.jpg'
 
+import waffle_1 from './img/waffle/1.png'
+import waffle_2 from './img/waffle/2.png'
+import waffle_3 from './img/waffle/3.png'
+import waffle_4 from './img/waffle/4.png'
+import waffle_5 from './img/waffle/5.png'
+import waffle_6 from './img/waffle/6.png'
+import waffle_7 from './img/waffle/7.png'
+import waffle_8 from './img/waffle/8.png'
+import waffle_9 from './img/waffle/9.png'
+import waffle_10 from './img/waffle/10.png'
+import waffle_11 from './img/waffle/11.png'
+import waffle_12 from './img/waffle/12.png'
+import waffle_13 from './img/waffle/13.png'
+import waffle_14 from './img/waffle/14.png'
+import waffle_15 from './img/waffle/15.png'
+import waffle_16 from './img/waffle/16.png'
+import waffle_17 from './img/waffle/17.png'
+import waffle_18 from './img/waffle/18.png'
+import waffle_19 from './img/waffle/19.png'
 
 export type ItemData = {
     id: number
@@ -76,335 +93,85 @@ export type ItemData = {
 }
 
 export const items:ItemData[] = [
-    {
-        id:1,
-        name: 'Dewdrop Leaf',
-        image: shirt_1,
-        description: 'A leaf'
-    },
-    {
-        id:2,
-        name: "Rural Bike",
-        image: shirt_2,
-        description: 'Boke'
-    },
-    {
-        id:3,
-        name: "Train Ride",
-        image: shirt_3,
-        description: 'Chhu Chu'
-    },
-    {
-        id:4,
-        name: "Citylines",
-        image: shirt_4,
-        description: 'lights and action'
-    },
-    {
-        id:5,
-        name: "Horse",
-        image: shirt_5,
-        description: 'Bojack'
-    },
-    {
-        id:6,
-        name: "Plant",
-        image: shirt_6,
-        description: 'I am groot'
-    },
-    {
-        id:7,
-        name: "Plant",
-        image: shirt_7,
-        description: 'I am groot'
-    },
-    {
-        id:8,
-        name: "Plant",
-        image: shirt_8,
-        description: 'I am groot'
-    },
-    {
-        id:9,
-        name: "Plant",
-        image: shirt_9,
-        description: 'I am groot'
-    },
-    {
-        id:10,
-        name: "Plant",
-        image: shirt_10,
-        description: 'I am groot'
-    },
-    {
-        id:11,
-        name: 'Dewdrop Leaf',
-        image: shirt_11,
-        description: 'A leaf'
-    },
-    {
-        id:12,
-        name: "Rural Bike",
-        image: shirt_12,
-        description: 'Boke'
-    },
-    {
-        id:13,
-        name: "Train Ride",
-        image: shirt_13,
-        description: 'Chhu Chu'
-    },
-    {
-        id:14,
-        name: "Citylines",
-        image: shirt_14,
-        description: 'lights and action'
-    },
-    {
-        id:15,
-        name: "Horse",
-        image: shirt_15,
-        description: 'Bojack'
-    },
-    {
-        id:16,
-        name: "Plant",
-        image: shirt_16,
-        description: 'I am groot'
-    },
-    {
-        id:17,
-        name: "Plant",
-        image: shirt_17,
-        description: 'I am groot'
-    },
-    {
-        id:18,
-        name: "Plant",
-        image: shirt_18,
-        description: 'I am groot'
-    },
-    {
-        id:19,
-        name: "Plant",
-        image: shirt_19,
-        description: 'I am groot'
-    },
-    {
-        id:20,
-        name: "Plant",
-        image: shirt_20,
-        description: 'I am groot'
-    },
-    {
-        id:21,
-        name: 'Dewdrop Leaf',
-        image: shirt_21,
-        description: 'A leaf'
-    },
-    {
-        id:22,
-        name: "Rural Bike",
-        image: shirt_22,
-        description: 'Boke'
-    },
-    {
-        id:23,
-        name: "Train Ride",
-        image: shirt_23,
-        description: 'Chhu Chu'
-    },
-    {
-        id:24,
-        name: "Citylines",
-        image: book_1,
-        description: 'lights and action'
-    },
-    {
-        id:25,
-        name: "Horse",
-        image: book_2,
-        description: 'Bojack'
-    },
-    {
-        id:26,
-        name: "Plant",
-        image: book_3,
-        description: 'I am groot'
-    },
-    {
-        id:27,
-        name: "Plant",
-        image: book_4,
-        description: 'I am groot'
-    },
-    {
-        id:28,
-        name: "Plant",
-        image: book_5,
-        description: 'I am groot'
-    },
-    {
-        id:29,
-        name: "Plant",
-        image: book_6,
-        description: 'I am groot'
-    },
-    {
-        id:30,
-        name: "Plant",
-        image: book_7,
-        description: 'I am groot'
-    },
-    {
-        id:31,
-        name: 'Dewdrop Leaf',
-        image: book_8,
-        description: 'A leaf'
-    },
-    {
-        id:32,
-        name: "Rural Bike",
-        image: book_9,
-        description: 'Boke'
-    },
-    {
-        id:33,
-        name: "Train Ride",
-        image: book_10,
-        description: 'Chhu Chu'
-    },
-    {
-        id:34,
-        name: "Citylines",
-        image: book_11,
-        description: 'lights and action'
-    },
-    {
-        id:35,
-        name: "Horse",
-        image: book_12,
-        description: 'Bojack'
-    },
-    {
-        id:36,
-        name: "Plant",
-        image: book_13,
-        description: 'I am groot'
-    },
-    {
-        id:37,
-        name: "Plant",
-        image: other_work_1,
-        description: 'I am groot'
-    },
-    {
-        id:38,
-        name: "Plant",
-        image: other_work_2,
-        description: 'I am groot'
-    },
-    {
-        id:39,
-        name: "Plant",
-        image: other_work_3,
-        description: 'I am groot'
-    },
-
+    {id:1,name: 'Dewdrop Leaf',image: shirt_1,description: 'A leaf'},
+    {id:2,name: "Rural Bike",image: shirt_2,description: 'Boke'},
+    {id:3,name: "Train Ride",image: shirt_3,description: 'Chhu Chu'},
+    {id:4,name: "Citylines",image: shirt_4,description: 'lights and action'},
+    {id:5,name: "Horse",image: shirt_5,description: 'Bojack'},
+    {id:6,name: "Plant",image: shirt_6,description: 'I am groot'},
+    {id:7,name: "Plant",image: shirt_7,description: 'I am groot'},
+    {id:8,name: "Plant",image: shirt_8,description: 'I am groot'},
+    {id:9,name: "Plant",image: shirt_9,description: 'I am groot'},
+    {id:10,name: "Plant",image: shirt_10,description: 'I am groot'},
+    {id:11,name: 'Dewdrop Leaf',image: shirt_11,description: 'A leaf'},
+    {id:12,name: "Rural Bike",image: shirt_12,description: 'Boke'},
+    {id:13,name: "Train Ride",image: shirt_13,description: 'Chhu Chu'},
+    {id:14,name: "Citylines",image: shirt_14,description: 'lights and action'},
+    {id:15,name: "Horse",image: shirt_15,description: 'Bojack'},
+    {id:16,name: "Plant",image: shirt_16,description: 'I am groot'},
+    {id:17,name: "Plant",image: shirt_17,description: 'I am groot'},
+    {id:18,name: "Plant",image: shirt_18,description: 'I am groot'},
+    {id:19,name: "Plant",image: shirt_19,description: 'I am groot'},
+    {id:20,name: "Plant",image: shirt_20,description: 'I am groot'},
+    {id:21,name: 'Dewdrop Leaf',image: shirt_21,description: 'A leaf'},
+    {id:22,name: "Rural Bike",image: shirt_22,description: 'Boke'},
+    {id:23,name: "Train Ride",image: shirt_23,description: 'Chhu Chu'},
+    {id:24,name: "Citylines",image: book_1,description: 'lights and action'},
+    {id:25,name: "Horse",image: book_2,description: 'Bojack'},
+    {id:26,name: "Plant",image: book_3,description: 'I am groot'},
+    {id:27,name: "Plant",image: book_4,description: 'I am groot'},
+    {id:28,name: "Plant",image: book_5,description: 'I am groot'},
+    {id:29,name: "Plant",image: book_6,description: 'I am groot'},
+    {id:30,name: "Plant",image: book_7,description: 'I am groot'},
+    {id:31,name: 'Dewdrop Leaf',image: book_8,description: 'A leaf'},
+    {id:32,name: "Rural Bike",image: book_9,description: 'Boke'},
+    {id:33,name: "Train Ride",image: book_10,description: 'Chhu Chu'},
+    {id:34,name: "Citylines",image: book_11,description: 'lights and action'},
+    {id:35,name: "Horse",image: book_12,description: 'Bojack'},
+    {id:36,name: "Plant",image: book_13,description: 'I am groot'},
+    {id:37,name: "Plant",image: other_work_1,description: 'I am groot'},
+    {id:38,name: "Plant",image: other_work_2,description: 'I am groot'},
+    {id:39,name: "Plant",image: other_work_3,description: 'I am groot'},
 ]
 
 export const hobbyItems:ItemData[] = [
-    {
-        id:1,
-        name: '',
-        image: lens_1,
-        description: ''
-    },
-    {
-        id:2,
-        name: "",
-        image: lens_2,
-        description: ''
-    },
-    {
-        id:3,
-        name: "",
-        image: lens_3,
-        description: ''
-    },
-    {
-        id:4,
-        name: '',
-        image: lens_4,
-        description: ''
-    },
-    {
-        id:5,
-        name: "",
-        image: lens_5,
-        description: ''
-    },
-    {
-        id:6,
-        name: "",
-        image: lens_6,
-        description: ''
-    },
-    {
-        id:7,
-        name: '',
-        image: lens_7,
-        description: ''
-    },
-    {
-        id:8,
-        name: "",
-        image: lens_8,
-        description: ''
-    },
-    {
-        id:9,
-        name: '',
-        image: lens_9,
-        description: ''
-    },
-    {
-        id:10,
-        name: "",
-        image: lens_10,
-        description: ''
-    },
-    {
-        id:11,
-        name: '',
-        image: lens_11,
-        description: ''
-    },
-    {
-        id:12,
-        name: "",
-        image: lens_12,
-        description: ''
-    },
-    {
-        id:13,
-        name: "",
-        image: lens_13,
-        description: ''
-    },
-    {
-        id:14,
-        name: "",
-        image: lens_14,
-        description: ''
-    },
-    {
-        id:15,
-        name: "",
-        image: lens_15,
-        description: ''
-    },
+    {id:1 ,name: '',image: lens_1 ,description: ''},
+    {id:2 ,name: "",image: lens_2 ,description: ''},
+    {id:3 ,name: "",image: lens_3 ,description: ''},
+    {id:4 ,name: '',image: lens_4 ,description: ''},
+    {id:5 ,name: "",image: lens_5 ,description: ''},
+    {id:6 ,name: "",image: lens_6 ,description: ''},
+    {id:7 ,name: '',image: lens_7 ,description: ''},
+    {id:8 ,name: "",image: lens_8 ,description: ''},
+    {id:9 ,name: '',image: lens_9 ,description: ''},
+    {id:10,name: "",image: lens_10,description: ''},
+    {id:11,name: '',image: lens_11,description: ''},
+    {id:12,name: "",image: lens_12,description: ''},
+    {id:13,name: "",image: lens_13,description: ''},
+    {id:14,name: "",image: lens_14,description: ''},
+    {id:15,name: "",image: lens_15,description: ''},
 ]
 
-export const canvaPhotoshopItems:ItemData[] = []
+export const canvaPhotoshopItems:ItemData[] = [
+    {id:1 ,name: "",image: waffle_1 ,description: ''},
+    {id:2 ,name: "",image: waffle_2 ,description: ''},
+    {id:3 ,name: "",image: waffle_3 ,description: ''},
+    {id:4 ,name: "",image: waffle_4 ,description: ''},
+    {id:5 ,name: "",image: waffle_5 ,description: ''},
+    {id:6 ,name: "",image: waffle_6 ,description: ''},
+    {id:7 ,name: "",image: waffle_7 ,description: ''},
+    {id:8 ,name: "",image: waffle_8 ,description: ''},
+    {id:9 ,name: "",image: waffle_9 ,description: ''},
+    {id:10,name: "",image: waffle_10,description: ''},
+    {id:11,name: "",image: waffle_11,description: ''},
+    {id:12,name: "",image: waffle_12,description: ''},
+    {id:13,name: "",image: waffle_13,description: ''},
+    {id:14,name: "",image: waffle_14,description: ''},
+    {id:15,name: "",image: waffle_15,description: ''},
+    {id:16,name: "",image: waffle_16,description: ''},
+    {id:17,name: "",image: waffle_17,description: ''},
+    {id:18,name: "",image: waffle_18,description: ''},
+    {id:19,name: "",image: waffle_19,description: ''},
+]
+
 export const printMediaItems:ItemData[] = []
